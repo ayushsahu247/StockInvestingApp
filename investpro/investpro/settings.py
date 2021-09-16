5a11381909456f214a90cb9601a7de8c3f344871
ayushsahu247 <ayush.cbs1@gmail.com>
2021-09-17 03:10:49 +0530

    For heroku deployment

--- investpro/investpro/settings.py
+++ investpro/investpro/settings.py
@@ -146,7 +146,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
